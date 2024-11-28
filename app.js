b5e39ac2d7f5e38f32344d1c6d413274f62ac970
cklgaming <colinklassey33@gmail.com>
2024-11-27 21:43:46 -0500

    continue to work on adding logins

--- app.js
+++ app.js
@@ -7,12 +7,22 @@ const PORT = process.env.PORT;
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+let filePath;
+
 const server = http.createServer(async (req, res) => {
+    console.log(req.method, req.url);
     if (req.method === 'GET' && req.url === '/') {
-        let filePath = path.join(__dirname, 'public', 'index.html');
-        res.setHeader('Content-Type', 'text/html');
-        res.write(await fs.readFile(filePath));
+        filePath = path.join(__dirname, 'public', 'index.html');
+    } else if (req.method === 'GET' && req.url === '/addLogin.html') {
+        filePath = path.join(__dirname, 'public', 'addLogin.html');
     } else {
         throw new Error('Server Error');
     }
+    const data = await fs.readFile(filePath);
+    res.setHeader('Content-Type', 'text/html');
+    res.write(data);
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
--- public/addLogin.js
+++ public/addLogin.js
@@ -1,6 +1,11 @@
+import fs from 'fs/promises';
+import { encrypt } from '../encrypt.js';
+import { decrypt } from '../decrypt.js';
+
 const USERNAME = document.querySelector('#username');
 const PASSWORD = document.querySelector('#password');
 const SUBMIT = document.querySelector('#submit');
+const MSG = document.querySelector('#msg');
 
 SUBMIT.onclick = submitFunc;
 
@@ -8,5 +13,16 @@ const submitFunc = () => {
     const usernameSaved = USERNAME.value;
     const passwordSaved = PASSWORD.value;
 
-    
+    try {
+        let [encryptUser, encryptPass] = encrypt(usernameSaved, passwordSaved);
+        fs.appendFile('./logins.csv', `\n${encryptUser}, ${encryptPass}`);
+        MSG.style.visibility = 'inline';
+        MSG.innerHTML = 'Login added successfully!';
+        console.log('Login added.');
+    } catch(err) {
+        console.log(err);
+        MSG.style.visibility = 'inline';
+        MSG.innerHTML = `An error occurred. Please try again later.`;
+        return 2;
+    }
 }
