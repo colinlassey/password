cfa04f9834ff40b64b638c14e8dc1b329d0ff021
cklgaming <colinklassey33@gmail.com>
2024-12-05 16:57:33 -0500

    started addPasswords.html

---
+++ public/addPasswords.js
@@ -0,0 +1,8 @@
+const NAME = document.querySelector('#name');
+const URL = document.querySelector('#url');
+const USERNAME = document.querySelector('#username');
+const PASSWORD = document.querySelector('#password');
+const SUGGEST = document.querySelector('#suggest');
+const SUGGESTION = document.querySelector('#suggestion');
+
+
